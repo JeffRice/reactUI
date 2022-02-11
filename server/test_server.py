847fbfeef3cf2255384e83ca589ee340f2ccc7f5
JXM029Y <jacob.miles@nexteraenergy.com>
2022-02-11 00:53:44 -0500

    added test skeletons

---
+++ server/test_server.py
@@ -0,0 +1,38 @@
+import pytest
+from server.server import create_app
+
+
+def test_login_requires_valid_input():
+    pass
+
+def test_login_rejects_invalid_password():
+    pass
+
+def test_login_returns_token():
+    pass
+
+def test_list_authorizes():
+    pass
+
+def test_list():
+    pass
+
+def test_create_calc_authorizes():
+    pass
+
+def test_create_calc():
+    pass
+
+def test_calc_detail_authorizes():
+    pass
+
+def test_calc_detail():
+    pass
+
+def test_cancel_calc_authorizes():
+    pass
+
+def test_cancel_calc():
+    pass
+
+
