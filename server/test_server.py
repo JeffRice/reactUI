4e9769291c9c705b01f05543232bc418e73fdd90
JXM029Y <jacob.miles@nexteraenergy.com>
2022-02-12 12:40:29 -0500

    added test client fixutres for test

--- server/test_server.py
+++ server/test_server.py
@@ -1,8 +1,51 @@
 import pytest
+from contextlib import contextmanager
 from server.server import create_app
 
 
-def test_login_requires_valid_input():
+@contextmanager
+def client(auth: bool):
+    app = create_app(auth=auth)
+    with app.test_client() as client:
+        yield client
+    
+@pytest.fixture
+def client():
+    with client(auth=True) as c:
+        yield c
+
+@pytest.fixture
+def client_no_auth():
+    with client(auth=False) as c:
+        yield c
+    
+@pytest.fixture
+def client_with_login():
+    with client_auth() as c:
+        response = c.post("/login", data={
+            "username": "fred",
+            "password": "password"
+        })
+        token = response['token']
+        yield ClientWithHeaders(headers={'x-auth': token})
+
+
+class ClientWithHeaders:
+    def __init__(self, client, headers):
+        self.client = client
+        self.headers = headers
+
+    def get(self, route):
+        return self.client(route, headers=self.headers)
+
+    def post(self, route, data):
+        return self.client(route, headers=self.headers, data=data)
+
+
+def test_login_requires_valid_input(client):
+    pass
+
+def test_login_rejects_if_no_auth(client_no_auth):
     pass
 
 def test_login_rejects_invalid_password():
@@ -14,10 +57,16 @@ def test_login_returns_token():
 def test_list_authorizes():
     pass
 
+def test_list_no_auth():
+    pass
+
 def test_list():
     pass
 
 def test_create_calc_authorizes():
+    pass
+
+def test_create_calc_no_auth():
     pass
 
 def test_create_calc():
@@ -26,10 +75,16 @@ def test_create_calc():
 def test_calc_detail_authorizes():
     pass
 
+def test_calc_detail_no_auth():
+    pass
+
 def test_calc_detail():
     pass
 
 def test_cancel_calc_authorizes():
+    pass
+
+def test_calcel_calc_no_auth():
     pass
 
 def test_cancel_calc():
